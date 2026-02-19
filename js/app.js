b2e5657b432ccea50c6bd33ee97d3be347778d55
smsj <soren@spangsberg.net>
2026-02-19 10:22:35 +0100

    just an update

--- js/app.js
+++ js/app.js
@@ -1,21 +1,3 @@
-const apiURL = "https://api.chucknorris.io/jokes/random";
 
-const getNextJoke = async () => {
-  
-  const res = await fetch(apiURL);
-  const joke = await res.json();
 
-  const content = document.getElementById('content');
-  content.innerText = `"${joke.value}"`;
-  content.classList.add("fst-italic");
-};
 
-//get first joke at initialization
-getNextJoke();
-
-jokesForm.addEventListener("submit", (e) => {
-  //preventing default
-  e.preventDefault();
-
-  getNextJoke();  
-});
